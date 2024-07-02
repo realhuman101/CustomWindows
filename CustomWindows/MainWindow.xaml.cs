9be45bf75aed32aedc214ecd8c8a10efc45edfe8
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-02 12:53:25 +0800

    fix syntax error

--- CustomWindows/MainWindow.xaml.cs
+++ CustomWindows/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace CustomWindows
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window {
         public MainWindow()
         {
             InitializeComponent();
